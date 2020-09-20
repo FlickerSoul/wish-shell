5d51ed4e49b159253c0030550eb828928db96ea2
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-09-19 21:27:55 -0700

    updated parallel command tests

--- test_parallel_commands.c
+++ test_parallel_commands.c
@@ -1,7 +1,7 @@
 #include "parallel_commands.h"
 #include "test_utils.h"
 
-void test_parse_command() {
+void test_parse_command_helper() {
     command_array* cmd = parse_command_(strdup("a b c"));
     assert(compare_string(cmd->commands[0], "a"));
     assert(compare_string(cmd->commands[1], "b"));
@@ -9,10 +9,33 @@ void test_parse_command() {
 }
 
 void test_parallel_commands() {
-    
+    parallel_commands* pc = quick_new_parallel_commands();
+    pc = new_parallel_commands(1, 0);
+    assert(pc->current == 0);
+    assert(pc->length == 1);
+
+    parse_command(&pc, strdup("a b c"));
+    assert(pc->current == 1);
+    assert(pc->length == 2);
+
+    command_array* cmd = pc->command_arrays[0];
+    assert(compare_string(cmd->commands[0], "a"));
+    assert(compare_string(cmd->commands[1], "b"));
+    assert(compare_string(cmd->commands[2], "c"));
+
+    parse_command(&pc, strdup("d e f"));
+    assert(pc->current == 2);
+    assert(pc->length == 4);
+
+    cmd = pc->command_arrays[1];
+    assert(compare_string(cmd->commands[0], "d"));
+    assert(compare_string(cmd->commands[1], "e"));
+    assert(compare_string(cmd->commands[2], "f"));
+
 }
 
 int main() {
-    test_parse_command();
+    test_parse_command_helper();
+    test_parallel_commands();
     test_parallel_commands();
 }
