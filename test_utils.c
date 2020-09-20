d242979e141aba83c250c3e4f11504f897548ce5
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-09-19 21:19:01 -0700

    added test_utils

---
+++ test_utils.c
@@ -0,0 +1,5 @@
+#include "test_utils.h"
+
+bool compare_string(const char* str1, const char* str2) {
+    return !strcmp(str1, str2);
+}
---
+++ test_utils.h
@@ -0,0 +1,6 @@
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+bool compare_string(const char* str1, const char* str2);
