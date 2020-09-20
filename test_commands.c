e42d2490b936c76a4ff9c0dc0b99252dbd9a3a5e
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-09-20 00:03:06 -0700

    updated tests

--- test_commands.c
+++ test_commands.c
@@ -3,22 +3,30 @@
 
 void test_commands() {
     command_array* c = quick_new_command_arr();
-    c = new_command_arr(1, 0);
+    free_all_commands_and_arr(&c);
+    c = new_command_arr(1, 0, NULL);
     assert(c != NULL);
     assert(c->length == 1);
     assert(c->current == 0);
 
-    char* dummy_command = "command";
-    push_command(&c, strdup(dummy_command));
+    char* dummy_command = strdup("command");
+    char* dummy_command2 = strdup("command2");
+    push_command(&c, dummy_command);
     assert(compare_string(dummy_command, c->commands[0]));
     assert(c->length == 2);
     assert(c->current == 1);
 
-    push_command(&c, strdup(dummy_command));
-    assert(compare_string(dummy_command, c->commands[1]));
+    char* dummy_out_name = strdup("std_out");
+    put_std_out(c, dummy_out_name);
+    assert(compare_string(c->std_out, dummy_out_name));
+
+    push_command(&c, dummy_command2);
+    assert(compare_string(dummy_command2, c->commands[1]));
     assert(c->length == 4);
     assert(c->current == 2);
 
+    assert(compare_string(c->std_out, dummy_out_name));
+    
     free_all_commands_and_arr(&c);
     assert(c == NULL);
 }
