7acb21c6ed05d95632044b75ac2a7b14a03f82c4
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-09-20 00:43:31 -0700

    added parse operator test

--- test_parallel_commands.c
+++ test_parallel_commands.c
@@ -1,15 +1,50 @@
 #include "parallel_commands.h"
 #include "test_utils.h"
 
+command_array* parse_simple_command_helper(char* new_line) {
+    command_array* command_arr = quick_new_command_arr();
+    char* last_part = new_line;
+    char* sep = NULL;
+    while ((sep = strtok_r(last_part, " ", &last_part)) != NULL) {
+        // when there is nothing in the the last part
+        // sep == NULL; do while makes sure that last 
+        // ele in arr in NULL
+        push_command(&command_arr, strdup(sep));
+    }
+    wrap_up_command(&command_arr);
+    free(new_line);
+    return command_arr;
+}
+
 void test_parse_command_helper() {
-    command_array* cmd = parse_command_(strdup("a b c"));
+    char* new_line = strdup("a b c");
+    command_array* cmd = parse_simple_command_helper(new_line);
     assert(compare_string(cmd->commands[0], "a"));
     assert(compare_string(cmd->commands[1], "b"));
     assert(compare_string(cmd->commands[2], "c"));
+    free_all_commands_and_arr(&cmd);
+}
+
+void test_parse_operator() {
+    char* new_line = strdup("a b c");
+    command_array* cmd = parse_simple_command_helper(new_line);
+
+    char* last_part = "test";
+    command_array* new_cmd = parse_operator(cmd, '&', &last_part);
+    assert(new_cmd != cmd);
+    free_all_commands_and_arr(&new_cmd);
+
+    new_cmd = parse_operator(cmd, '>', &last_part);
+    assert(new_cmd == cmd);
+    assert(compare_string("test", cmd->std_out));
+
+    free_all_commands_and_arr(&cmd);
 }
 
 void test_parallel_commands() {
     parallel_commands* pc = quick_new_parallel_commands();
+    free_parallel_commands_and_all(&pc);
+
     pc = new_parallel_commands(1, 0);
     assert(pc->current == 0);
     assert(pc->length == 1);
@@ -35,10 +70,12 @@ void test_parallel_commands() {
     assert(compare_string(cmd->commands[2], "f"));
     assert(cmd->commands[3] == NULL);
     assert(cmd->current == 4);
+
+    free_parallel_commands_and_all(&pc);
 }
 
 int main() {
     test_parse_command_helper();
-    test_parallel_commands();
+    test_parse_operator();
     test_parallel_commands();
 }
